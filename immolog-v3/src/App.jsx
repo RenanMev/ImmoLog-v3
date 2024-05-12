a6be2c6dbb1430170d5bec03e58adb17d372e6e0
Renan mev <125215421+RenanMev@users.noreply.github.com>
2024-05-12 20:40:57 -0300

    Revert "Merge branch 'main' of https://github.com/RenanMev/ImmoLog-v3"
    This reverts commit 49c03429aaa512974d557b2d7af041068aec6aec, reversing
    changes made to 5892f4befb2936c6d8378e004532bb0c2d7d6f2e.

---
+++ immolog-v3/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { Dashboard } from './Pages/dashboard'
+
+function App() {
+  return (
+    <>
+    <Dashboard/>
+    </>
+  )
+}
+
+export default App
