fc0fc1e37dc66ef27e9060cf908af99f3782ea3d
Renan mev <125215421+RenanMev@users.noreply.github.com>
2024-06-07 15:51:28 -0300

    imp-main-loginAutomatico

--- src/Auth/LoginPage.jsx
+++ src/Auth/LoginPage.jsx
@@ -9,7 +9,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import ImgLogo from "@/assets/Logoverde.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import AlertErrPass from "../Pages/components-pages/alert/AlertErrPass";
@@ -26,9 +26,36 @@ export function LoginForm() {
     setPassword(e.target.value);
   };
 
-  const handleLoginFormSubmit = (e) => {
+  const validLogin = async () => {
+    const idAsth = localStorage.getItem('idAsth');
+    if (!idAsth) {
+        return;
+    }
+    
+    try {
+        const res = await axios.post("http://localhost:3333/validLogin", { id: idAsth });
+        if (res.status === 200) {
+            localStorage.setItem("user", res.data[0].email);
+            localStorage.setItem("user", res.data[0].username);
+            localStorage.setItem("idAsth", res.data[0].id);
+            window.location.href = "/dashboard";
+        } else {
+            console.error("Erro no login:", res.data.message);
+        }
+    } catch (error) {
+        console.error("Erro na solicitação:", error);
+    }
+}
+
+
+
+  useEffect( ()=>{
+    validLogin()
+  },[])
+
+  const handleLoginFormSubmit = async (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3333/login', {
+   await axios.post('http://localhost:3333/login', {
       email: email,
       password: password
     })
@@ -40,6 +67,7 @@ export function LoginForm() {
           }, 20000);
         } if (res.data.status === 200) {
          localStorage.setItem("user", res.data.mensage.username);
+         localStorage.setItem("idAsth", res.data.mensage.id);
           window.location.href = "/dashboard";
         }
       })
@@ -94,9 +122,6 @@ export function LoginForm() {
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              {/* <Button variant="outline" className="w-full">
-                Login with Google
-              </Button> */}
             </div>
           </form>
           <div className="mt-4 text-center text-sm">
